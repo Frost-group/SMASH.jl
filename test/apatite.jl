ced36aa2d49c1b090dfefc9f33d809241a3a8251
Jarvist Moore Frost <jarvist@gmail.com>
2023-08-01 16:38:43 +0100

    Starting to move towards ingesting AtomsIO trajectories.

--- test/apatite.jl
+++ test/apatite.jl
@@ -5,4 +5,10 @@ t = load_trajectory("P6Pb9Cu1O26H2.vasp_md.traj") # traj -> ASE traj via AtomsIO
 
 filter( a -> a.atomic_symbol == :Cu, t[2].particles)
 
+using LinearAlgebra
+[norm(Pb.position-Cus[1].position) for Pb in Pbs]
+# doesn't appear to use minimum image convention - just Angstrom distances within unit cell
+
+# something like this, but tripping over Unitful
+SMASH.minimd(Pbs[1].position,Cus[1].position,t[1].bounding_box)
 
