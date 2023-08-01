35d8c20d34e2a4bd0476d59afc1325f626ac5166
Documenter.jl <documenter@juliadocs.github.io>
2023-08-01 09:22:41 +0000

    build based on 45235cd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SMASH","category":"page"},{"location":"#SMASH","page":"Home","title":"SMASH","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SMASH.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SMASH]","category":"page"}]
+}
